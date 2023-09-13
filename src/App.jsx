5b9f729aed9990624cc46d892eafb729fb0f3f80
shivansh13579 <shivansh0975@gmail.com>
2023-09-13 16:24:35 +0530

    use callback hooks

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-full max-w-md mx-auto my-8 bg-gray-800 shadow-md rounded-lg py-3 px-4 text-orange-600'>
+    <h1 className='text-white text-center my-3'>Password Generator</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
